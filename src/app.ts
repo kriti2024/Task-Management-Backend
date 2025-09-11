b8b430d3711e0f263564d5cbec4609a35eef1403
Kriti Tripathi <tripathikriti483@gmail.com>
2025-09-11 18:03:45 +0545

    add image of member

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import router from "./routes/index.js";
+import path from "path";
+
 const app = express();
 const PORT = 5000;
+import cors from "cors";
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use(express.json());
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use("/api", router);
 app.listen(PORT, (error) => {
   if (!error) {
