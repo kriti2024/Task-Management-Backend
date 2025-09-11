cd3fa11fe6028bc9f7dbbb7248b18dae90363e2c
Kriti Tripathi <tripathikriti483@gmail.com>
2025-09-11 18:04:50 +0545

    add router for login and task

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import userRouter from "./user.route.js";
+import authRouter from "./auth.route.js";
+import taskRouter from "./task.route.js";
 
 const router = Router();
 
 router.use("/user", userRouter);
+router.use("/auth", authRouter);
+router.use("/task", taskRouter);
 export default router;
