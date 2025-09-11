2cd95166be210e8bc03d69a78ffaacafb6dc386b
Kriti Tripathi <tripathikriti483@gmail.com>
2025-09-11 18:06:07 +0545

    add authetication , authorization and image

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,15 +5,28 @@ import {
   updateUser,
   deleteUser,
 } from "../controllers/user.controller.js";
-
+import { authenticate, authorize } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/upload.middleware.js";
 const userRouter = Router();
 
-userRouter.post("/", createUser);
+userRouter.post(
+  "/",
+  authenticate,
+  authorize(["ADMIN"]),
+  upload.single("image"),
+  createUser
+);
 
-userRouter.get("/", getAllUser);
+userRouter.get("/", authenticate, authorize(["ADMIN"]), getAllUser);
 
-userRouter.put("/:id", updateUser);
+userRouter.put(
+  "/:id",
+  authenticate,
+  authorize(["ADMIN"]),
+  upload.single("image"),
+  updateUser
+);
 
-userRouter.delete("/:id", deleteUser);
+userRouter.delete("/:id", authenticate, authorize(["ADMIN"]), deleteUser);
 
 export default userRouter;
